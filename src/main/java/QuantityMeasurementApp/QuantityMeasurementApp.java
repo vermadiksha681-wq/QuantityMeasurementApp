10a6f500e207a7ce9576ebb3c21c36d8a94c654c
vermadiksha681-wq <vermadiksha681@gmail.com>
2026-04-01 10:12:33 +0530

    updated with static values

--- src/main/java/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -1,73 +1,16 @@
 package QuantityMeasurementApp;
 
-
-
 import com.quantity.measurement.enums.LengthUnit;
-
 import com.quantity.measurement.model.QuantityLength;
-
-import java.util.Scanner;
-
-
 
 public class QuantityMeasurementApp {
 
-
-
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-
-
-        try {
-
-            System.out.print("Enter first value: ");
-
-            double value1 = Double.parseDouble(scanner.nextLine());
-
-            System.out.print("Enter first unit (FEET, INCH): ");
-
-            String unit1 = scanner.nextLine();
-
-
-
-            System.out.print("Enter second value: ");
-
-            double value2 = Double.parseDouble(scanner.nextLine());
-
-            System.out.print("Enter second unit (FEET, INCH): ");
-
-            String unit2 = scanner.nextLine();
-
-
-
-            // Convert input to uppercase before using valueOf
-
-            QuantityLength length1 = new QuantityLength(value1, LengthUnit.valueOf(unit1.toUpperCase()));
-
-            QuantityLength length2 = new QuantityLength(value2, LengthUnit.valueOf(unit2.toUpperCase()));
-
-
-
-            if (length1.equals(length2)) {
-
-                System.out.println("The two lengths are equal.");
-
-            } else {
-
-                System.out.println("The two lengths are NOT equal.");
-
-            }
-
-
-
-        } catch (IllegalArgumentException e) {
-
-            System.out.println("Error: " + e.getMessage());
-
-        }
+        
+            // Static values directly define kar diye
+            QuantityLength length1 = new QuantityLength(1.0, LengthUnit.FEET);
+            QuantityLength length2 = new QuantityLength(12.0, LengthUnit.INCH);
 
     }
-
 }
